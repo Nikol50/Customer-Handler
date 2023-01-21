37d10bbf3265bd3b78c258279f44da5e261d9985
Nikol50 <nikol234.barah@gmail.com>
2023-01-21 22:45:15 +0200

    updated env

--- server/env/Scripts/activate.bat
+++ server/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\magshimim\Documents\Nikol\My Web\Customer-Handler\server\env
+set VIRTUAL_ENV=E:\NikolStuff\workers project\Customer-Handler\server\env
 
 if not defined PROMPT set PROMPT=$P$G
 
